2e8fc85b07de8664c72a52bb03e2d10e50ac8bf9
lortiz24 <ortizfrancoluisjavier@gmail.com>
2025-05-24 17:26:40 -0500

    Se añade un hook de debounce para optimizar la gestión de cambios en el filtro de nombre en el componente CharacterFilters. Se actualiza el manejo de eventos para reflejar el valor debounced y se ajustan estilos en los componentes de selección.

--- src/components/character/CharacterFilters.tsx
+++ src/components/character/CharacterFilters.tsx
@@ -1,7 +1,8 @@
 import { Box, TextField, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import { useState, useEffect, ChangeEvent } from 'react';
 import { CharacterFiltersValues } from '../../interfaces/character.interface';
 import { speciesOptions, genderOptions, statusOptions } from '../../constants/filter.constants';
-import { ChangeEvent } from 'react';
+import { useDebounce } from '../../hook/useDebounce';
 
 interface Props {
 	filters: CharacterFiltersValues;
@@ -9,13 +10,26 @@ interface Props {
 }
 
 export const CharacterFilters = ({ filters, onChange }: Props) => {
+	const [inputValue, setInputValue] = useState(filters.name);
+	const debouncedValue = useDebounce(inputValue, 500);
+
+	useEffect(() => {
+		if (debouncedValue !== filters.name) {
+			onChange({ ...filters, name: debouncedValue });
+		}
+	}, [debouncedValue]);
+
 	const handleChange = (
 		e:
 			| (Event & { target: { value: string; name: string } })
 			| ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
 	) => {
 		const { name, value } = e.target;
-		onChange({ ...filters, [name!]: value });
+		if (name === 'name') {
+			setInputValue(value as string);
+		} else {
+			onChange({ ...filters, [name!]: value });
+		}
 	};
 
 	return (
@@ -33,10 +47,11 @@ export const CharacterFilters = ({ filters, onChange }: Props) => {
 				label='Filter by name...'
 				variant='outlined'
 				size='small'
-				value={filters.name}
+				value={inputValue}
 				onChange={handleChange}
+				sx={{ width: 240 }}
 			/>
-			<FormControl size='small' sx={{ minWidth: 120 }}>
+			<FormControl size='small' sx={{ minWidth: 120, width: 240 }}>
 				<InputLabel>Species</InputLabel>
 				<Select name='species' value={filters.species} label='Species' onChange={handleChange}>
 					{speciesOptions.map((opt) => (
@@ -46,7 +61,7 @@ export const CharacterFilters = ({ filters, onChange }: Props) => {
 					))}
 				</Select>
 			</FormControl>
-			<FormControl size='small' sx={{ minWidth: 120 }}>
+			<FormControl size='small' sx={{ minWidth: 120, width: 240 }}>
 				<InputLabel>Gender</InputLabel>
 				<Select name='gender' value={filters.gender} label='Gender' onChange={handleChange}>
 					{genderOptions.map((opt) => (
@@ -56,7 +71,7 @@ export const CharacterFilters = ({ filters, onChange }: Props) => {
 					))}
 				</Select>
 			</FormControl>
-			<FormControl size='small' sx={{ minWidth: 120 }}>
+			<FormControl size='small' sx={{ minWidth: 120, width: 240 }}>
 				<InputLabel>Status</InputLabel>
 				<Select name='status' value={filters.status} label='Status' onChange={handleChange}>
 					{statusOptions.map((opt) => (
